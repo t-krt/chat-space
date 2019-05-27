2bc57e5420e15003d296ad85ed744f5a8655f7fa
t-krt <cest1la6vieg@gmail.com>
2019-05-27 23:02:20 +0900

    Add: buildHTML関数の作成
    非同期通信の成功時に作成するHTM・組み立てる関数を作成しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,18 @@
 $(function(){
+  function buildHTML(message) {
+    var messageName = `<li class="chat-messages__name">${ message.user_name }</li>`;
+    var messageTime = `<li class="chat-messages__time">${ message.created_at }</li>`;
+    var messageContent = `<li class="chat-messages__text">${ message.content }</li>`;
+    var messageImage = `<div class="chat-messages__image">
+                          <img src="${ message.image }" > 
+                        </div>` 
+    var html = `<ul class="chat-messeages">
+                ${ messageName } 
+                ${ messageTime }`
+    html += (message.content) ? `${ messageContent }` : "";
+    html += (message.image) ? `${ messageImage }  </ul>` : `</ul>`;
+    return html
+  }
   $('#new-message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
