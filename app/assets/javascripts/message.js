84c59864ea60c2489ea4d06b8da6832f2074cc73
t-krt <cest1la6vieg@gmail.com>
2019-05-27 22:51:55 +0900

    Add: Ajaxで非同期通信化
    フォームの値を、messagesコントローラのcreateアクションに非同期通信で送るコードを記述しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,15 @@
 $(function(){
   $('#new-message').on('submit', function(e){
     e.preventDefault();
+    var formData = new FormData(this);
+    var url = window.location.href
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
