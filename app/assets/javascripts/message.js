b80180dd5c928afe8c51e7a885ea198bc290d591
t-krt <cest1la6vieg@gmail.com>
2019-05-27 23:29:14 +0900

    Fix: ビューの表示修正
    メッセージと画像が同時に表示された際に、横並びに表示されてしまっていたため、画像を<li>タグ内に入れることで修正しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,13 +3,13 @@ $(function(){
     var messageName = `<li class="chat-messages__name">${ message.user_name }</li>`;
     var messageTime = `<li class="chat-messages__time">${ message.created_at }</li>`;
     var messageContent = `<li class="chat-messages__text">${ message.content }</li>`;
-    var messageImage = `<div class="chat-messages__image">
+    var messageImage = `<li class="chat-messages__image">
                           <img src="${ message.image }" > 
-                        </div>` 
+                        </li>` 
     var html = `<ul class="chat-messeages">
                 ${ messageName } 
-                ${ messageTime }`
-    html += (message.content) ? `${ messageContent }` : "";
+                ${ messageTime }
+                ${ messageContent }`
     html += (message.image) ? `${ messageImage }  </ul>` : `</ul>`;
     return html
   }
