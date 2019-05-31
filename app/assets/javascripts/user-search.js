949b3a6947916d19c1b5965c49ec7107b5339b83
t-krt <cest1la6vieg@gmail.com>
2019-05-31 21:02:09 +0900

    Add: チャットメンバーの追加・削除機能の作成
    検索結果に表示されたチャットメンバーを追加、削除できるコードを記述しました。

--- app/assets/javascripts/user-search.js
+++ app/assets/javascripts/user-search.js
@@ -1,20 +1,43 @@
-$(function(){
+$(document).on("ready turbolinks:load", function() {
   var search_user_list = $('#user-search-result')
+  var group_members = $('.chat-group-users')
 
   function appendUser(user) {
-    var html = `<div class="chat-group-user clearfix">
+    var html = `<div class="chat-group-user clearfix" id="chat-group-user-8">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
                 </div>`
     search_user_list.append(html);
   }
 
-  function appendErrMsgToHTML(msg) {
+  function appendNoUserToHTML(msg) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ msg }</p>
                 </div>`
     search_user_list.append(html)
   }
+
+  function appendUserToGroup(user_id, user_name) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id="${ user_id }">削除</div>
+                </div>`
+    group_members.append(html)
+  }
+
+
+  $(document).on('click', ".user-search-add", function(){
+    var user_id = $(this).data('user-id')
+    var user_name =  $(this).data('user-name')
+    appendUserToGroup(user_id, user_name)
+    $(this).parent().remove();
+  });
+
+  $(document).on('click', ".user-search-remove", function(){
+    var user_id = $(this).data('user-id')
+    $(this).parent().remove();
+  });
 
   $('#user-search-field').on('keyup', function(e){
     var input = $('#user-search-field').val();
@@ -32,7 +55,7 @@ $(function(){
         });
       }
       else {
-        appendErrMsgToHTML('一致するユーザーが見つかりません')
+        appendNoUserToHTML('一致するユーザーが見つかりません')
       }
     })
     .fail(function() {
