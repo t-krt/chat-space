f621709139282aafe9fa1416183809d88d163c72
t-krt <cest1la6vieg@gmail.com>
2019-06-07 23:22:42 +0900

    Update: ユーザー検索結果の修正
    検索フォームの値が空になったときに、検索結果が空になるように修正しました。

--- app/assets/javascripts/user-search.js
+++ app/assets/javascripts/user-search.js
@@ -40,25 +40,28 @@ $(document).on("ready turbolinks:load", function() {
 
   $('#user-search-field').on('keyup', function(e){
     var input = $('#user-search-field').val();
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-    .done(function(users) {
-      $('#user-search-result').empty();
-      if(users.length !== 0) {
-        users.forEach(function(user){
-          appendUserToSearchResult(user);
-        });
-      }
-      else {
-        appendNoUserToSearchResult('一致するユーザーが見つかりません')
-      }
-    })
-    .fail(function() {
-      alert('ユーザー検索に失敗しました')
-    })
+    if (input.length !== 0) {
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+      .done(function(users) {
+        user_search_result.empty();
+        if(users.length !== 0) {
+          users.forEach(function(user){
+            appendUserToSearchResult(user);
+          });
+        } else {
+          appendNoUserToSearchResult('一致するユーザーが見つかりません')
+        }
+      })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました')
+      })
+    } else {
+      user_search_result.empty();
+    }
   });
 });
