637b545de171dae5b10785bafa388720fe6a2239
t-krt <cest1la6vieg@gmail.com>
2019-06-07 23:27:40 +0900

    Update: 変数名の修正
    JSの命名規則に従い、変数名をキャメルケースに統一しました。

--- app/assets/javascripts/user-search.js
+++ app/assets/javascripts/user-search.js
@@ -1,40 +1,40 @@
 $(document).on("ready turbolinks:load", function() {
-  var user_search_result = $('#user-search-result')
-  var group_users = $('.chat-group-users')
+  var userSearchResult = $('#user-search-result')
+  var groupUsers = $('.chat-group-users')
 
   function appendUserToSearchResult(user) {
     var html = `<div class="chat-group-user clearfix" id="chat-group-user-8">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
                 </div>`
-    user_search_result.append(html);
+    userSearchResult.append(html);
   }
 
   function appendNoUserToSearchResult(msg) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ msg }</p>
                 </div>`
-    user_search_result.append(html)
+    userSearchResult.append(html)
   }
 
-  function appendUserToGroup(user_id, user_name) {
+  function appendUserToGroup(userId, userName) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
-                  <p class='chat-group-user__name'>${ user_name }</p>
-                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id="${ user_id }">削除</div>
+                  <input name='group[user_ids][]' type='hidden' value='${ userId }'>
+                  <p class='chat-group-user__name'>${ userName }</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id="${ userId }">削除</div>
                 </div>`
-    group_users.append(html)
+    groupUsers.append(html)
   }
 
   $(document).on('click', ".user-search-add", function(){
-    var user_id = $(this).data('user-id')
-    var user_name =  $(this).data('user-name')
-    appendUserToGroup(user_id, user_name)
+    var userId = $(this).data('user-id')
+    var userName =  $(this).data('user-name')
+    appendUserToGroup(userId, userName)
     $(this).parent().remove();
   });
 
   $(document).on('click', ".user-search-remove", function(){
-    var user_id = $(this).data('user-id')
+    var userId = $(this).data('user-id')
     $(this).parent().remove();
   });
 
@@ -48,7 +48,7 @@ $(document).on("ready turbolinks:load", function() {
         dataType: 'json'
       })
       .done(function(users) {
-        user_search_result.empty();
+        userSearchResult.empty();
         if(users.length !== 0) {
           users.forEach(function(user){
             appendUserToSearchResult(user);
@@ -61,7 +61,7 @@ $(document).on("ready turbolinks:load", function() {
         alert('ユーザー検索に失敗しました')
       })
     } else {
-      user_search_result.empty();
+      userSearchResult.empty();
     }
   });
 });
